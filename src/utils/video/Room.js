8486d750004abdff57db77d2b5e10f6638eb8c5b
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-02-28 19:44:14 +0000

    Changed Room to use (...args) => callback(...args) for binding

--- src/utils/video/Room.js
+++ src/utils/video/Room.js
@@ -13,12 +13,6 @@ import Peer from './Peer';
     => See Peer.js for events that can be listened for on object 'user'
 */
 
-/*
-  REVIEW:
-    Need to consider async when emitting events, might be relevant
-    See about bable having arrow function syntax for autobinding
-     - LINK: https://medium.com/quick-code/react-quick-tip-use-class-properties-and-arrow-functions-to-avoid-binding-this-to-methods-29628aca2e25
-*/
 class Room {
   constructor() {
     // super();
@@ -37,20 +31,11 @@ class Room {
       'room-userconnect': [],
     };
 
-    // Socket Event handles
-    this.onSocketNotification = this.onSocketNotification.bind(this);
-
-    // Local Room Event Handles
-    this.onRoomClose = this.onRoomClose.bind(this);
-    this.onRoomNewPeer = this.onRoomNewPeer.bind(this);
-    this.onRoomRequest = this.onRoomRequest.bind(this);
-    this.onRoomNotify = this.onRoomNotify.bind(this);
-
     // Connect event listeners
-    this.msRoom.on('close', this.onRoomClose);
-    this.msRoom.on('newpeer', this.onRoomNewPeer);
-    this.msRoom.on('request', this.onRoomRequest);
-    this.msRoom.on('notify', this.onRoomNotify);
+    this.msRoom.on('close', () => this.onRoomClose());
+    this.msRoom.on('newpeer', mediasoupPeer => this.onRoomNewPeer(mediasoupPeer));
+    this.msRoom.on('request', ...args => this.onRoomRequest(...args));
+    this.msRoom.on('notify', notification => this.onRoomNotify(notification));
   }
 
   join(client) {
@@ -60,7 +45,11 @@ class Room {
 
     // Connect our local and remote room through a socket
     this.socket = io(url, { query });
-    this.socket.on('mediasoup-notification', this.socketOnNotification);
+
+    // Establish callback for notifications through socket
+    this.socket.on('mediasoup-notification', (notification) => {
+      this.socketOnNotification(notification);
+    });
 
     // Join the room
     return this.msRoom.join(uid)
@@ -128,15 +117,17 @@ class Room {
   }
 
   onRoomClose() {
-    // Not fully sure how much to clean up.
-    // Probably best to just create a new Room and close socket.
+    // Probably best to just close socket and create a new Room if needed
     this.emitRoomclose();
     this.socket.close();
   }
 
   onRoomNewPeer(newpeer) {
+    // Create a new peer and add it to the list
     const peer = new Peer(newpeer, this);
     this.peers.set(peer.uid, peer);
+
+    // Set up callback and for when 'user-disconnect' and emit 'room-userconenct'
     peer.on('user-disconnect', ({ uid }) => this.peers.delete(uid));
     this.emit('room-userconnect', peer);
   }
