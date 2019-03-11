abe55c488557359ed50b7e58e40f11a75a0f99e4
inglec <inglec@tcd.ie>
2019-03-11 13:48:08 +0000

    404: Rewrite component

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom';
 import './styles';
 
 const NotFound = () => (
-  <div className="centered">
-    <div className="centered">
-      <h1>404: Not Found</h1>
-      <p>The page you are requesting does not exist.</p>
+  <div className="page notfound">
+    <div className="notfound-container">
+      <div className="message">
+        <h1>404: Not Found</h1>
+        <h5>The page you are requesting does not exist</h5>
+      </div>
+      <Link to="/">Return home</Link>
     </div>
-    <Link to="/">Return to the home page</Link>
   </div>
 );
 
