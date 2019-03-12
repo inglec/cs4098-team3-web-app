92bbca5953fdd72a4fcff2668acd1675e7c25b53
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-03-12 21:02:03 +0000

    Added props for width and height on video

--- src/components/Review/Video/index.jsx
+++ src/components/Review/Video/index.jsx
@@ -4,17 +4,27 @@ import PropTypes from 'prop-types';
 /*
   Might need to be a class for functionality purposes
 */
-const Video = ({ videoSrc }) => (
+const Video = ({ videoSrc, height, width }) => (
   <div className="review-video">
-    <video key={videoSrc} className="video" controls>
+    <video key={videoSrc} height={height} width={width} className="video" controls>
       <source src={videoSrc} type="video/mp4" />
     </video>
 
   </div>
 );
 
+// Linter doesn't like { ..., height=480, width=640 } form of optional params
+// https://stackoverflow.com/questions/47774695/react-functional-component-default-props-vs-default-parameters
+// Turns out these props will be checked against propTypes while default values wont
+Video.defaultProps = {
+  height: 480,
+  width: 640,
+};
+
 Video.propTypes = {
   videoSrc: PropTypes.string.isRequired,
+  height: PropTypes.number,
+  width: PropTypes.number,
 };
 
 export default Video;
