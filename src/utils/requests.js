f3619da677709f07d4065a29a7558b00b4b6fdb5
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-03-12 20:10:36 +0000

    Moved test data retrieval to requests.js

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,5 +1,7 @@
 // import axios from 'axios';
+import { patients } from 'app-root/data/test/patients';
 
+const videoSrc = 'http://localhost:8080/data/test/small.mp4'
 // TODO
 // const AUTH_SERVER_URL = 'https://';
 
@@ -14,5 +16,29 @@ export const authenticate = (uid, password) => (
         reject(Error('bad credentials'));
       }
     }, 2000);
+  })
+);
+
+export const patientsByGroup = groupId => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.05) {
+        resolve(patients);
+      } else {
+        reject(Error(`Could not retrieve patients for group ${groupId}`));
+      }
+    }, 500);
+  })
+);
+
+export const getVideoSrcById = videoId => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.05) {
+        resolve(videoSrc);
+      } else {
+        reject(Error(`Could not retrieve video for videoId ${videoId}`));
+      }
+    }, 500);
   })
 );
