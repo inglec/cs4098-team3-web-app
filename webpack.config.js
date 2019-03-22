e1200c95fe7ad32f30c58a67f984b4fe340d3464
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-03-22 18:39:35 +0000

    Changed two configs to the one and use env.production to choose config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env) => {
+  const configURI = (env.config === 'prod')
+    ? 'src/config.prod.json'
+    : 'src/config.dev.json';
+  return {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: { loader: 'babel-loader' },
+        },
+        {
+          test: /\.(c|le)ss/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'less-loader',
+          ],
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.less'],
+      alias: {
+        'app-root': __dirname,
+        'app-components': path.resolve(__dirname, 'src/components'),
+        'app-containers': path.resolve(__dirname, 'src/containers'),
+        'app-redux': path.resolve(__dirname, 'src/redux'),
+        'app-utils': path.resolve(__dirname, 'src/utils'),
+        'test-data': path.resolve(__dirname, 'data/test'),
+        'app-config': path.resolve(__dirname, configURI),
+      },
+    },
+
+    devServer: { historyApiFallback: true },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+    ],
+
+  };
+};
