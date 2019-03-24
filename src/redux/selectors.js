bf8ed831dd7a52fe9727eba5cf305d3f3f15882a
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-03-24 23:34:32 +0000

    Created selectors for inidividual session chats

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,6 +3,10 @@ import { createSelector } from 'reselect';
 
 export const getAuth = state => state.auth;
 
+// REVIEW: Tried to change to 'getChats' and 'state.chats' to reflect
+// that it's an array of chats but I couldn't get it to work for redux
+// Likewise getChatUsers is a little misleading, getChatsUsers?
+
 export const getChat = state => state.chat;
 
 export const getSessions = state => state.sessions;
@@ -10,8 +14,8 @@ export const getSessions = state => state.sessions;
 // Array of unique users in chat for each session
 export const getChatUsers = createSelector(
   getChat,
-  chat => (
-    mapValues(chat, (session) => {
+  chats => (
+    mapValues(chats, (session) => {
       const uids = new Set();
       session.forEach(message => uids.add(message.sender));
 
@@ -19,3 +23,10 @@ export const getChatUsers = createSelector(
     })
   ),
 );
+
+// Might not be the correct way to do it
+// But the other options are index into them in mapStateToProps or in the component
+// Here it's more reusable
+export const getSessionMessages = (state, sessionId) => state.chat[sessionId];
+
+export const getSessionUsers = (state, sessionId) => getChatUsers(state)[sessionId];
