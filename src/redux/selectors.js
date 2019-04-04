5989f7da97126802b4280105bde596e46802a355
inglec <inglec@tcd.ie>
2019-04-04 22:23:01 +0100

    Selectors: Add selectors for active, past and future sessions

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { mapValues } from 'lodash/object';
+import { mapValues, pickBy } from 'lodash/object';
 import { createSelector } from 'reselect';
 
 export const getAuth = state => state.auth;
@@ -12,14 +12,23 @@ export const getUsers = state => state.users;
 export const getUser = createSelector(getUsers, getAuth, (users, { uid }) => users[uid]);
 
 // Array of unique users in chat for each session
-export const getChatUsers = createSelector(
-  getChat,
-  chat => (
-    mapValues(chat, (session) => {
-      const uids = new Set();
-      session.forEach(message => uids.add(message.sender));
+export const getChatUsers = createSelector(getChat, chat => (
+  mapValues(chat, (session) => {
+    const uids = new Set();
+    session.forEach(message => uids.add(message.sender));
 
-      return Array.from(uids);
-    })
-  ),
-);
+    return Array.from(uids);
+  })
+));
+
+export const getActiveSessions = createSelector(getSessions, sessions => (
+  pickBy(sessions, ({ endTime, startTime }) => startTime < new Date().getTime() && !endTime)
+));
+
+export const getPastSessions = createSelector(getSessions, sessions => (
+  pickBy(sessions, ({ endTime }) => endTime < new Date().getTime())
+));
+
+export const getFutureSessions = createSelector(getSessions, sessions => (
+  pickBy(sessions, ({ startTime }) => startTime > new Date().getTime())
+));
