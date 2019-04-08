4211eedd09ade091c1b8e913b1a48872cb02fc26
inglec <inglec@tcd.ie>
2019-04-08 19:23:50 +0100

    Remove redundant code from requests.js

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,10 +1,3 @@
-// import axios from 'axios';
-
-import { patients } from 'test-data/patients';
-import { video } from 'test-data/videoinfo';
-
-const defaultPicSrc = 'http://localhost:8080/data/test/nopic.png';
-
 // TODO: Actually do authentication.
 export const authenticate = (uid, password) => (
   // axios.post(AUTH_SERVER_URL, { uid, password })
@@ -27,36 +20,6 @@ export const updateProfile = (token, fields) => (
         resolve();
       } else {
         reject(Error('missing fields'));
-      }
-    }, 500);
-  })
-);
-
-export const patientsByGroup = groupId => (
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (Math.random() >= 0.00) {
-        // Linter doesn't seem to like forEach for changing values
-        patients.forEach((patient) => {
-          if (!patient.imageSrc) {
-            patient.imageSrc = defaultPicSrc;
-          }
-        });
-        resolve(patients);
-      } else {
-        reject(Error(`Could not retrieve patients for group ${groupId}`));
-      }
-    }, 500);
-  })
-);
-
-export const getVideoById = videoId => (
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (Math.random() >= 0.00) {
-        resolve(video);
-      } else {
-        reject(Error(`Could not retrieve video for videoId ${videoId}`));
       }
     }, 500);
   })
